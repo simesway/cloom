a6ab4e73781bdc13bb1261739dc250d13c93d11b
Simon Eiber <eiber.simon@web.de>
2024-06-12 19:28:23 +0200

    added cmd line handling

--- examples/asset-viewer.lisp
+++ examples/asset-viewer.lisp
@@ -4,8 +4,9 @@
 (load "~/quicklisp/setup.lisp")
 (asdf:load-system "cloom")
 
-(defparameter *wad-filepath* "../../data/DOOM1.WAD")
+(defparameter *wad-filepath* nil)
 (defparameter *data-sequence* nil)
+(defvar *mode* nil)
 
 (defun update-and-wait (renderer)
   (view-renderer:update renderer)
@@ -15,18 +16,47 @@
   (let ((path (uiop:file-exists-p path-string)))
     (if path
 	(setf *wad-filepath* path)
-	(format t "given filepath '~a' does not exist~%" path-string))))
+	(if path-string
+	    (format t "given filepath '~a' does not exist~%" path-string)))))
 
 (defun handle-arguments ()
   (let* ((cmdline (uiop:command-line-arguments))
 	 (length  (length cmdline))
 	 (path    (first cmdline))
-	 (mode    (second cmdline)))
+	 (modes   (cdr cmdline)))
+    (set-filepath path)
     (if (< length 1)
-	(format t "asset-viewer [filepath] [--textures|--sprites|--flats|--all]~%"))
-    (set-filepath path)))
+	(format t "asset-viewer [filepath] [--textures|--sprites|--flats|--images|--all]~%"))
+    (if (< length 2)
+	(setf modes (list "--all")))
+    modes))
 	  
-      
+(defun collect-data (modes wad-reader asset-data)
+  (let ((data '())
+	(textures nil)
+	(sprites nil)
+	(flats nil)
+	(images nil))
+    (dolist (mode modes)
+      (cond
+	((string= mode "--textures") (setf textures t))
+	((string= mode "--sprites")  (setf sprites t))
+	((string= mode "--images")   (setf images t))
+	((string= mode "--flats")    (setf flats t))
+	((string= mode "--all")
+	 (setf textures t)
+	 (setf sprites t)
+	 (setf images t)
+	 (setf flats t))))
+    (if textures
+	(setq data (append data (asset-data::textures asset-data))))
+    (if sprites
+	(setq data (append data (asset-data::sprites  asset-data))))
+    (if flats
+	(setq data (append data (asset-data::flats    asset-data))))
+    (if images
+	(setq data (append data (asset-data::get-images wad-reader))))
+    data))
 
 (defun view-loop (renderer data)
   (let ((updated t)
@@ -70,16 +100,25 @@
 		      (sleep 0.01)))))))
 
 (defun main ()
-  (handle-arguments)
-  (let* ((reader (wad-reader:wad-reader-init *wad-filepath*))
-	 (asset-data (asset-data::get-asset-data reader))
-	 (renderer (view-renderer:view-renderer-init asset-data))
-	 (data (asset-data::sprites asset-data))
-	 (images (asset-data::get-images reader)))
-    (view-loop renderer data)
-    (view-renderer:view-renderer-close)
-    (wad-reader:wad-reader-close reader)))
+  
+  (let ((modes  (handle-arguments)))
+
+    (when (not *wad-filepath*)
+      (return-from main))
+
+    (let* ((reader (wad-reader:wad-reader-init *wad-filepath*))
+	   (asset-data (asset-data::get-asset-data reader))
+	   (data (collect-data modes reader asset-data)))
+
+      (when (not data)
+	(format t "No data found~%")
+	(return-from main))
+
+      (let ((renderer (view-renderer:view-renderer-init asset-data)))
+	(view-loop renderer data)
+	(view-renderer:view-renderer-close)
+	(wad-reader:wad-reader-close reader)))))
 
 	
 
-(sb-ext:save-lisp-and-die "asset-viewer2" :toplevel #'main :executable t)
+(sb-ext:save-lisp-and-die "asset-viewer" :toplevel #'main :executable t)
